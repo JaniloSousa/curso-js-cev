df75e99f7aca326ef19fda4af147cedeefed6420
Janilo Paixão de Sousa <janilopaixao2@gmail.com>
2023-11-21 10:03:40 -0200

    continuando meus estudos em js
    terminei de estudar dom e vi a primeira parte de condicoes

---
+++ aula11/script01.js
@@ -0,0 +1,12 @@
+/*
+JAVASCRIPT - CONDIÇÕES
+CONDIÇÃO COMPOSTA
+*/
+
+var pais = "Brasil";
+
+if (pais == "Brasil") { // bloco se verdade
+    console.log("Você é Brasileiro!");
+} else { // bloco se falso
+    console.log("Você é estrangeiro!");
+}
---
+++ aula11/script02.js
@@ -0,0 +1,14 @@
+/*
+JAVASCRIPT - CONDIÇÕES
+CONDIÇÃO SIMPLES
+*/
+
+var vel = 80;
+
+console.log(`Sua velocidade é de ${vel}km/h!`);
+
+if (vel > 60) { // as condições simples tem apenas o bloco verdade
+    console.log(`Você foi MULTADO por excesso de velocidade!`);
+}
+
+console.log(`Dirija sempre com cinto de segurança!`);
