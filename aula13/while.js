ac9bc1190ee66848db680a17569a43e3102f7967
Janilo Paixão de Sousa <janilopaixao2@gmail.com>
2023-11-23 17:07:20 -0200

    estudar a primeira parte de repetições

---
+++ aula13/while.js
@@ -0,0 +1,24 @@
+/*
+ESTRUTURAS DE REPETIÇÕES EM JAVASCRIPT
+WHILE (lê-se "enquanto") -> testa a condição e depois faz o que tem que ser feito
+DO WHILE (lê-se "faça ... enquanto") -> faz o que tem que ser feito e depois testa a condição
+*/
+
+var contador = 1; // variável contadora
+while(contador <= 10) { // condição
+    console.log(`Passo ${contador}`); // loop
+    contador++; // não se esqueça de atualizar a variável se não cai em um loop infinito
+}
+
+/*
+console.log("Passo 1");
+console.log("Passo 2");
+console.log("Passo 3");
+console.log("Passo 4");
+console.log("Passo 5");
+console.log("Passo 6");
+console.log("Passo 7");
+console.log("Passo 8");
+console.log("Passo 9");
+console.log("Passo 10");
+*/
