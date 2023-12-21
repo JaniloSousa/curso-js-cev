521efc42d25cc2f37e199e00be86f309e21910e6
Janilo Paixão de Sousa <janilopaixao2@gmail.com>
2023-12-21 14:50:28 -0200

    terminei meus estudos em repetições

---
+++ aula14/exe01.js
@@ -0,0 +1,15 @@
+/*
+JAVASCRIPT - ESTRUTURAS DE REPETIÇÕES
+ESTRUTURA DE REPETIÇÃO FOR
+* SINTAXE:
+    for(inicio;teste;incremento) {
+        ... bloco de código ... 
+    }
+*/
+console.log("Seu programa vai começar!");
+for(var c = 1; c <= 10; c++) {
+    console.log(`Repetição ${c}`);
+}
+console.log("FIM!");
+
+// DICA: ultilize o laço de repetição 'for' quando você souber de ante-mão quantas vezes o laço irá se repetir!
---
+++ aula14/exe02.js
@@ -0,0 +1,21 @@
+/*
+vamos fazer a contagem de 1 até 10 nos dois casos possíveis de laço
+*/
+
+console.log("Iniciando o laço while");
+
+c=1;
+while(c<=10) {
+    console.log(c);
+    c++;
+}
+
+console.log("Fim do laço while");
+console.log("======================================");
+console.log("Iniciando o laço for");
+
+for(c=1;c<=10;c++) {
+    console.log(c);
+}
+
+console.log("Fim do laço for");
